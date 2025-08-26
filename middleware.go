22bdb57986115206a247cb6429d44cade2d46924
iami317 <mlh_210@163.com>
2025-08-26 16:34:41 +0800

    init

--- middleware.go
+++ middleware.go
@@ -61,11 +61,7 @@ func readResponseBody(resp *Response, c *Client) error {
 	}
 	resp.Body = bodyBytes
 	defer func() {
-		fmt.Println("=========关闭 http", resp.GetUrl().String())
-		err = resp.RawResponse.Body.Close()
-		if err != nil {
-			fmt.Println("=======", err)
-		}
+		_ = resp.RawResponse.Body.Close()
 	}()
 	return nil
 }
